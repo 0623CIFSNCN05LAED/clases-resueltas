5c5f6766bd77475a674126e0e4a72330fac18a62
LuiosD <luisdartayet@gmail.com>
2023-11-06 20:40:21 -0300

    clase-45 init

---
+++ clase-45/public/js/moviesAdd.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  let titulo = document.querySelector(".moviesAddTitulo");
+  let formulario = document.querySelector("#formulario");
+  let article = document.querySelector("article");
+  titulo.innerHTML = "AGREGAR PELÍCULA";
+  titulo.classList.add("titulo");
+  article.classList.add("fondoTransparente");
+  formulario.classList.add("fondoCRUD");
+};
