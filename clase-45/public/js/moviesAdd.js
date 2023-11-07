9cfda8604823671cd309c7113fde681bfd554529
LuiosD <luisdartayet@gmail.com>
2023-11-06 23:00:32 -0300

    clase-45 desafio 2

--- clase-45/public/js/moviesAdd.js
+++ clase-45/public/js/moviesAdd.js
@@ -10,7 +10,7 @@ window.onload = function () {
   //https://stackoverflow.com/questions/1484506/random-color-generator
   function getRandomColor() {
     const letters = "0123456789ABCDEF";
-    const color = "#";
+    let color = "#";
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
@@ -20,4 +20,30 @@ window.onload = function () {
   titulo.addEventListener("mouseover", function () {
     titulo.style.color = getRandomColor();
   });
+
+  const palabraSecreta = "secret";
+  let estadoSecreto = 0;
+
+  const input = document.querySelector("#titulo");
+
+  input.addEventListener("keydown", (e) => {
+    const letra = e.key;
+
+    console.log(letra);
+
+    if (palabraSecreta[estadoSecreto] === letra) {
+      estadoSecreto++;
+    } else {
+      estadoSecreto = 0;
+    }
+
+    if (estadoSecreto === palabraSecreta.length) {
+      estadoSecreto = 0;
+      setTimeout(() => {
+        alert("SECRETO!!!");
+      }, 500);
+    }
+  });
+
+
 };
