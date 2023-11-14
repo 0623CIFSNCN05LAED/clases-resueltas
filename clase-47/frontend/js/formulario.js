43b5288800863e9a856fed2db7613c2633a59dcc
LuiosD <luisdartayet@gmail.com>
2023-11-13 21:44:20 -0300

    agrega instrucciones desafio 1b

--- clase-47/frontend/js/formulario.js
+++ clase-47/frontend/js/formulario.js
@@ -1,2 +1,14 @@
 window.onload = () => {
+  let movie = {};
+
+  // AGREGAR VALORES A LOS CAMPOS
+  // 1
+  // obtener la pelicula
+  // ¿de dónde? de la URL correspondiente de la API local...
+  // ¿Cómo? usando fet...
+  // ACA VA EL FETCH!!
+
+  // 2
+  // capturar el title
+  // piso el value con el titulo de la movie de la API
 }
