97fc78f97bcd877321a6e9235cf3c8c057ed7861
LuiosD <luisdartayet@gmail.com>
2023-11-22 22:25:07 -0300

    first components

---
+++ clase-50/dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+
+import SearchWrap from './components/Search-wrap'
+
+
+function App() {
+
+  return (
+    <SearchWrap/>
+  )
+}
+
+export default App
---
+++ clase-50/dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import './Dashboard.css'
+import SearchWrap from './Search-wrap';
+
+function Dashboard(){
+    return (
+    <div className="dashboard">
+        hola
+        <SearchWrap/>
+    </div>
+        )
+}
+
+export default Dashboard;
